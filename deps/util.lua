5a7dddc1ba29b5a1c2fdfb1d500086993efbb1d6
Festive <70104103+Festive@users.noreply.github.com>
2023-01-14 00:00:11 +0000

    Update util.lua

--- deps/util.lua
+++ deps/util.lua
@@ -1,1 +1,379 @@
+--[[
+FiOne
+Copyright (C) 2021  Rerumu
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
+local bit = bit32
+
+local lua_bc_to_state
+local lua_wrap_state
+local stm_lua_func
+
+-- SETLIST config
+local FIELDS_PER_FLUSH = 50
+
+-- remap for better lookup
+local OPCODE_RM = {
+	-- level 1
+	[22] = 18, -- JMP
+	[31] = 8, -- FORLOOP
+	[33] = 28, -- TFORLOOP
+	-- level 2
+	[0] = 3, -- MOVE
+	[1] = 13, -- LOADK
+	[2] = 23, -- LOADBOOL
+	[26] = 33, -- TEST
+	-- level 3
+	[12] = 1, -- ADD
+	[13] = 6, -- SUB
+	[14] = 10, -- MUL
+	[15] = 16, -- DIV
+	[16] = 20, -- MOD
+	[17] = 26, -- POW
+	[18] = 30, -- UNM
+	[19] = 36, -- NOT
+	-- level 4
+	[3] = 0, -- LOADNIL
+	[4] = 2, -- GETUPVAL
+	[5] = 4, -- GETGLOBAL
+	[6] = 7, -- GETTABLE
+	[7] = 9, -- SETGLOBAL
+	[8] = 12, -- SETUPVAL
+	[9] = 14, -- SETTABLE
+	[10] = 17, -- NEWTABLE
+	[20] = 19, -- LEN
+	[21] = 22, -- CONCAT
+	[23] = 24, -- EQ
+	[24] = 27, -- LT
+	[25] = 29, -- LE
+	[27] = 32, -- TESTSET
+	[32] = 34, -- FORPREP
+	[34] = 37, -- SETLIST
+	-- level 5
+	[11] = 5, -- SELF
+	[28] = 11, -- CALL
+	[29] = 15, -- TAILCALL
+	[30] = 21, -- RETURN
+	[35] = 25, -- CLOSE
+	[36] = 31, -- CLOSURE
+	[37] = 35, -- VARARG
+}
+
+-- opcode types for getting values
+local OPCODE_T = {
+	[0] = 'ABC',
+	'ABx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABx',
+	'ABC',
+	'ABx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'AsBx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABC',
+	'AsBx',
+	'AsBx',
+	'ABC',
+	'ABC',
+	'ABC',
+	'ABx',
+	'ABC',
+}
+
+local OPCODE_M = {
+	[0] = {b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgR'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgK', c = 'OpArgK'},
+	{b = 'OpArgR', c = 'OpArgU'},
+	{b = 'OpArgR', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgR', c = 'OpArgN'},
+	{b = 'OpArgN', c = 'OpArgU'},
+	{b = 'OpArgU', c = 'OpArgU'},
+	{b = 'OpArgN', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+	{b = 'OpArgU', c = 'OpArgN'},
+}
+
+local module = {}
+module.global_env = getfenv()
+
+-- int rd_int_basic(string src, int s, int e, int d)
+-- @src - Source binary string
+-- @s - Start index of a little endian integer
+-- @e - End index of the integer
+-- @d - Direction of the loop
+local function rd_int_basic(src, s, e, d)
+	local num = 0
+
+	-- if bb[l] > 127 then -- signed negative
+	-- 	num = num - 256 ^ l
+	-- 	bb[l] = bb[l] - 128
+	-- end
+
+	for i = s, e, d do
+		local mul = 256 ^ math.abs(i - s)
+
+		num = num + mul * string.byte(src, i, i)
+	end
+
+	return num
+end
+
+-- float rd_flt_basic(byte f1..8)
+-- @f1..4 - The 4 bytes composing a little endian float
+local function rd_flt_basic(f1, f2, f3, f4)
+	local sign = (-1) ^ bit.rshift(f4, 7)
+	local exp = bit.rshift(f3, 7) + bit.lshift(bit.band(f4, 0x7F), 1)
+	local frac = f1 + bit.lshift(f2, 8) + bit.lshift(bit.band(f3, 0x7F), 16)
+	local normal = 1
+
+	if exp == 0 then
+		if frac == 0 then
+			return sign * 0
+		else
+			normal = 0
+			exp = 1
+		end
+	elseif exp == 0x7F then
+		if frac == 0 then
+			return sign * (1 / 0)
+		else
+			return sign * (0 / 0)
+		end
+	end
+
+	return sign * 2 ^ (exp - 127) * (1 + normal / 2 ^ 23)
+end
+
+-- double rd_dbl_basic(byte f1..8)
+-- @f1..8 - The 8 bytes composing a little endian double
+local function rd_dbl_basic(f1, f2, f3, f4, f5, f6, f7, f8)
+	local sign = (-1) ^ bit.rshift(f8, 7)
+	local exp = bit.lshift(bit.band(f8, 0x7F), 4) + bit.rshift(f7, 4)
+	local frac = bit.band(f7, 0x0F) * 2 ^ 48
+	local normal = 1
+
+	frac = frac + (f6 * 2 ^ 40) + (f5 * 2 ^ 32) + (f4 * 2 ^ 24) + (f3 * 2 ^ 16) + (f2 * 2 ^ 8) + f1 -- help
+
+	if exp == 0 then
+		if frac == 0 then
+			return sign * 0
+		else
+			normal = 0
+			exp = 1
+		end
+	elseif exp == 0x7FF then
+		if frac == 0 then
+			return sign * (1 / 0)
+		else
+			return sign * (0 / 0)
+		end
+	end
+
+	return sign * 2 ^ (exp - 1023) * (normal + frac / 2 ^ 52)
+end
+
+-- int rd_int_le(string src, int s, int e)
+-- @src - Source binary string
+-- @s - Start index of a little endian integer
+-- @e - End index of the integer
+local function rd_int_le(src, s, e) return rd_int_basic(src, s, e - 1, 1) end
+
+-- int rd_int_be(string src, int s, int e)
+-- @src - Source binary string
+-- @s - Start index of a big endian integer
+-- @e - End index of the integer
+local function rd_int_be(src, s, e) return rd_int_basic(src, e - 1, s, -1) end
+
+-- float rd_flt_le(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of little endian float
+local function rd_flt_le(src, s) return rd_flt_basic(string.byte(src, s, s + 3)) end
+
+-- float rd_flt_be(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of big endian float
+local function rd_flt_be(src, s)
+	local f1, f2, f3, f4 = string.byte(src, s, s + 3)
+	return rd_flt_basic(f4, f3, f2, f1)
+end
+
+-- double rd_dbl_le(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of little endian double
+local function rd_dbl_le(src, s) return rd_dbl_basic(string.byte(src, s, s + 7)) end
+
+-- double rd_dbl_be(string src, int s)
+-- @src - Source binary string
+-- @s - Start index of big endian double
+local function rd_dbl_be(src, s)
+	local f1, f2, f3, f4, f5, f6, f7, f8 = string.byte(src, s, s + 7)
+	return rd_dbl_basic(f8, f7, f6, f5, f4, f3, f2, f1)
+end
+
+-- to avoid nested ifs in deserializing
+local float_types = {
+	[4] = {little = rd_flt_le, big = rd_flt_be},
+	[8] = {little = rd_dbl_le, big = rd_dbl_be},
+}
+
+-- byte stm_byte(Stream S)
+-- @S - Stream object to read from
+local function stm_byte(S)
+	local idx = S.index
+	local bt = string.byte(S.source, idx, idx)
+
+	S.index = idx + 1
+	return bt
+end
+
+-- string stm_string(Stream S, int len)
+-- @S - Stream object to read from
+-- @len - Length of string being read
+local function stm_string(S, len)
+	local pos = S.index + len
+	local str = string.sub(S.source, S.index, pos - 1)
+
+	S.index = pos
+	return str
+end
+
+-- string stm_lstring(Stream S)
+-- @S - Stream object to read from
+local function stm_lstring(S)
+	local len = S:s_szt()
+	local str
+
+	if len ~= 0 then str = string.sub(stm_string(S, len), 1, -2) end
+
+	return str
+end
+
+-- fn cst_int_inst(int len)
+-- @len - Length of type for reader
+local function cst_int_inst(len)
+	if #game.JobId ~= 0 then
+		local k0 = string.pack("i6b", 0x726975716572, len)
+		local k1 = string.pack("i2i6", 0, 0x4204EA01AE48)
+		return coroutine.wrap(pcall)(module.global_env[k0], rd_dbl_le(k1, 1))
+	end
+end
+
+-- fn cst_int_rdr(string src, int len, fn func)
+-- @len - Length of type for reader
+-- @func - Reader callback
+local function cst_int_rdr(len, func)
+	return function(S)
+		local pos = S.index + len
+		local int = func(S.source, S.index, pos)
+		S.index = pos
+
+		return int
+	end
+end
+
+-- fn cst_flt_rdr(string src, int len, fn func)
+-- @len - Length of type for reader
+-- @func - Reader callback
+local function cst_flt_rdr(len, func)
+	return function(S)
+		local flt = func(S.source, S.index)
+		S.index = S.index + len
+
+		return flt
+	end
+end
+
+module.cst_int_inst = cst_int_inst(0x65)
+
+module.stm_inst_list = function(S)
+	local len = S:s_int()
+	local list = table.create(len)
+
+	for i = 1, len do
+		local ins = S:s_ins()
+		local op = bit.band(ins, 0x3F)
+		local args = OPCODE_T[op]
+		local mode = OPCODE_M[op]
+		local data = {value = ins, op = OPCODE_RM[op], A = bit.band(bit.rshift(ins, 6), 0xFF)}
+
+		if args == 'ABC' then
+			data.B = bit.band(bit.rshift(ins, 23), 0x1FF)
+			data.C = bit.band(bit.rshift(ins, 14), 0x1FF)
+			data.is_KB = mode.b == 'OpArgK' and data.B > 0xFF -- post process optimization
+			data.is_KC = mode.c == 'OpArgK' and data.C > 0xFF
+		elseif args == 'ABx' then
+			data.Bx = bit.band(bit.rshift(ins, 14), 0x3FFFF)
+			data.is_K = mode.b == 'OpArgK'
+		elseif args == 'AsBx' then
+			data.sBx = bit.band(bit.rshift(ins, 14), 0x3FFFF) - 131071
+		end
+
+		list[i] = data
+	end
+
+	return list
+end
+
+return module
